c045b0ae062c0a654d7dbaa4afd5d6ef31f8a02f
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2023-04-28 23:19:19 +0200

    quête accomplie

---
+++ movieHandlers.js
@@ -0,0 +1,70 @@
+const database = require("./database.js");
+
+const getMovies = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movie]) => {
+      if (movie[0]) {
+        res.json(movie[0]);
+      } else {
+        res.status(404).send("Navré, nous n'avons pas cette référence.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => res.status(200).json(users))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([user]) => {
+      if (user[0]) {
+        res.status(200).json(user[0]);
+      } else {
+        res
+          .status(404)
+          .send(
+            "Cet identifiant n'est pas attribué, prenez-le et rejoignez nous."
+          );
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getMovies,
+  getMovieById,
+  getUsers,
+  getUsersById,
+};
