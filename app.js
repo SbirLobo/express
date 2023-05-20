655961ae7f6b77de5c3e09aaea58d0bcc99d4bdf
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2023-05-20 16:33:21 +0200

    express 04bis

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 
+const { validateMovie } = require("./validators.js");
+const { validateUser } = require("./userValidators.js");
+
 const app = express();
 
 app.use(express.json());
@@ -20,10 +23,10 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUsersById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", movieHandlers.postUser);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", movieHandlers.updateUser);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieHandlers.postUser);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUser);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", movieHandlers.deleteUser);
 
